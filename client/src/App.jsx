69b842f9d1a7384d3f603e8c690288e6bcad698c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-16 18:18:17 +0530

    input and button are added....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import io from "socket.io-client";
+import { nanoid } from "nanoid";
+//  no dotenv
+const socket = io("http://localhost:5000");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chatty App</h1>
+      <form action="">
+        <input
+          type="text"
+          name="chat"
+          placeholder="Send msg"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button type="submit">Send</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
