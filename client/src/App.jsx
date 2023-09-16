c8feb0d5fdde1897f47d89e253ba3ec7b2a901df
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-16 22:18:51 +0530

    facility to send the chat and display the messages is added .....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,29 @@ import { nanoid } from "nanoid";
 //  no dotenv
 const socket = io("http://localhost:5000");
 
+const userName = nanoid(4);
+
 function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
 
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { message, userName });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  }, [chat]);
+
   return (
     <>
       <h1>Chatty App</h1>
-      <form action="">
+
+      <form onSubmit={sendChat}>
         <input
           type="text"
           name="chat"
@@ -25,6 +40,13 @@ function App() {
         />
         <button type="submit">Send</button>
       </form>
+      {chat.map((payload, index) => {
+        return (
+          <p key={index}>
+            {payload.message} - <span>id : {payload.userName}</span>
+          </p>
+        );
+      })}
     </>
   );
 }
